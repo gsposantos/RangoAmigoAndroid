8489df280fbc427c2957e873178d3bd65c42f174
gsposantos <gsposantos@gmail.com>
2018-02-22 23:44:27 -0300

    Navegacao Cadastro Evento - Interface

--- app/src/main/java/com/example/guilherme/rangoamigo/fragments/EtapaDoisFragment.java
+++ app/src/main/java/com/example/guilherme/rangoamigo/fragments/EtapaDoisFragment.java
@@ -1,18 +1,32 @@
 package com.example.guilherme.rangoamigo.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.guilherme.rangoamigo.R;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EtapaDoisFragment extends Fragment {
+
+
+    private static final int PLACE_PICKER_REQUEST = 1;
+    private View view;
+    private TextInputLayout txtEndereco;
+    private TextInputLayout txtNomeLocal;
+    private Button btnVerMapa;
 
 
     public EtapaDoisFragment() {
@@ -24,7 +38,45 @@ public class EtapaDoisFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_etapadois, container, false);
+        view =  inflater.inflate(R.layout.fragment_etapadois, container, false);
+
+        txtNomeLocal = (TextInputLayout) view.findViewById(R.id.txtNomeLocal);
+        txtEndereco = (TextInputLayout) view.findViewById(R.id.txtEndereco);
+        btnVerMapa = (Button) view.findViewById(R.id.btnVerMapa);
+
+        btnVerMapa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    Intent intent = builder.build(getActivity());
+                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == PLACE_PICKER_REQUEST){
+
+            if (resultCode == getActivity().RESULT_OK){
+
+                Place place = PlacePicker.getPlace(getActivity(),data);
+
+                txtNomeLocal.getEditText().setText(place.getName());
+                txtEndereco.getEditText().setText(place.getAddress());
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/guilherme/rangoamigo/fragments/EtapaUmFragment.java
+++ app/src/main/java/com/example/guilherme/rangoamigo/fragments/EtapaUmFragment.java
@@ -1,19 +1,30 @@
 package com.example.guilherme.rangoamigo.fragments;
 
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.guilherme.rangoamigo.R;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EtapaUmFragment extends Fragment {
 
+
+    private View view;
+    private ImageButton imgEvento;
 
     public EtapaUmFragment() {
         // Required empty public constructor
@@ -24,7 +35,41 @@ public class EtapaUmFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_etapaum, container, false);
+        view = inflater.inflate(R.layout.fragment_etapaum, container, false);
+
+        imgEvento = (ImageButton) view.findViewById(R.id.imgEvento);
+        imgEvento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intentImg = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(Intent.createChooser(intentImg, "Selecione uma imagem"), 1234);
+            }
+        });
+
+        return view;
+    }
+
+    /* EVENTO RETORNO GALERIA */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(resultCode != getActivity().RESULT_CANCELED){
+
+            if(requestCode == 1234){
+
+                Uri imagemSelecioanda = data.getData();
+                try {
+                    InputStream inputStream = getActivity().getContentResolver().openInputStream(imagemSelecioanda);
+                    imgEvento.setBackground(Drawable.createFromStream(inputStream, imagemSelecioanda.toString()));
+                    //bAlterouImagem = true;
+                } catch (FileNotFoundException e) {
+                   // this.showAlert("Erro!", "Ocoreu um problema para carregar a imagem "+ imagemSelecioanda.toString());
+                }
+
+            }
+
+        }
     }
 
 }
